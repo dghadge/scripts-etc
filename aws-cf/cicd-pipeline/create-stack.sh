8be2068e0c50627ff0f2f177f163162b8acd46e2
Dnyanesh Ghadge <nyy021@gmail.com>
2017-05-10 10:52:34 -0400

    moved files to private bucket

--- aws-cf/cicd-pipeline/create-stack.sh
+++ aws-cf/cicd-pipeline/create-stack.sh
@@ -5,4 +5,4 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
-aws cloudformation create-stack --stack-name $1 --template-body file://codebuild-cpl-cd-cc.json --parameters file://codebuild-cpl-cd-cc-parameters.json --region us-east-1 --capabilities="CAPABILITY_IAM"
+aws cloudformation create-stack --stack-name $1 --template-body file://codepipeline-with-cc-cb-cd-steps.json --parameters file://codepipeline-with-cc-cb-cd-steps-parameters.json --region us-east-1 --capabilities="CAPABILITY_IAM"
